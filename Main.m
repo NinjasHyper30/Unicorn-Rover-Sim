d0705c8deebb54ee8d186595f587648c6e7b5999
Ninja <hariprime30@yahoo.com>
2024-03-18 13:57:45 +0530

    Complete sim v1
    An assumption that motors track the input is made. Further sim with motor model must be done

--- Main.m
+++ Main.m
@@ -12,3 +12,34 @@ r=0.35;
 %% Simulation
 open('UnicornSim.slx');
 sim('UnicornSim.slx');
+
+%% Ploting
+
+
+%ploting trajectory
+figure(1);
+subplot(1,2,1);
+plot(inTraj(:,1),inTraj(:,2));
+grid on;
+xlabel('X(meters)'); ylabel("Y(meters)");
+title('INPUT PATH');
+axis([-5 55 -5 55]);
+subplot(1,2,2);
+plot(outTraj(:,1),outTraj(:,2),'m--');
+grid on;
+xlabel('X(meters)'); ylabel("Y(meters)");
+title('OUTPUT PATH');
+axis([-5 55 -5 55]);
+
+%ploting orientation
+figure(2);
+subplot(1,2,1);
+plot(tout,inPsi);
+grid on;
+xlabel('t(seconds)'); ylabel("\psi (rad)");
+title('INPUT ORIENTATION');
+subplot(1,2,2);
+plot(tout,outPsi);
+grid on;
+xlabel('t(seconds)'); ylabel("\psi (rad)");
+title('OUTPUT ORIENTATION');
